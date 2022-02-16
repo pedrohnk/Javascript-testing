8a8548c5dd2562b524ddb310050c3c6a8f4ef42b
Pedro Lima <pedro.lima@bhz.jamef.com.br>
2022-02-15 21:50:54 -0300

    studies finished

---
+++ api.js
@@ -0,0 +1,47 @@
+const http = require("http")
+const DEFAULT_USER = { username: "PedroLima", password: "Xzin@12" }
+
+const routes = {
+    '/contact:get':(request, response) => {
+        response.write("Contact us page")
+        return response.end()
+    },
+
+    '/login:post': async(request, response) => {
+        for await (const data of request) {
+            const user = JSON.parse(data)
+            
+            if(user.username !== DEFAULT_USER.username || user.password !== DEFAULT_USER.password) {
+                response.writeHead(401)
+                response.write("Login failed!")
+
+                return response.end()
+            }
+
+            response.write("Logging has succeeded!")
+            return response.end()
+        }
+    },
+
+    default: (request, response) => {
+        response.write("Hello World")
+        return response.end()
+    }
+}
+
+const handler = async(request, response) => {
+    const { url, method } = request
+    const routeKey = `${url}:${method.toLowerCase()}`
+    const chosen = routes[routeKey] || routes.default
+
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    })
+
+    return chosen(request, response)
+}
+
+const app = http.createServer(handler)
+                .listen(3000, () => console.log(`App is listening on port: `, 3000))
+
+module.exports = app;
